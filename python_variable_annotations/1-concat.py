cd16d9cd147374fe8895031b5fb1086847c92a7a
moha88z <mohammed.alayda88@gmail.com>
2025-10-06 10:52:32 +0000

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module is for a function that concat 2 strings
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    this fucntion takes 2 str and adds them and retruns a string
+    """
+    return a + b
