e4c3adafafc9316c7e811735736bd04875d4ab49
moha88z <mohammed.alayda88@gmail.com>
2025-10-06 11:19:14 +0000

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module is for a function that floors a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    this fucntion takes a float and floors it and retruns an int
+    """
+    return int(n // 1)
