f8e08b6dd844917c89d9fa2ba4ce32af6720d13e
moha88z <mohammed.alayda88@gmail.com>
2025-10-06 10:32:51 +0000

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the module is for a function that adds 2 float num
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    this fucntion takes 2 floats and adds them and retruns a float
+    """
+    return a + b
