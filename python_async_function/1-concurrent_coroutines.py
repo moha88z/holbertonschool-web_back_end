9488dda3b6dbe2f52736371dce4521b98d984c83
moha88z <mohammed.alayda88@gmail.com>
2025-10-08 09:46:12 +0000

    1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""this module with afunction that sleeps with a random value """
+
+
+import asyncio
+import typing
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """a function that sleeps with random value"""
+
+    tasks = [wait_random(max_delay) for i in range(n)]
+    list_Delays = await asyncio.gather(*tasks)
+
+    return sorted(list_Delays)
