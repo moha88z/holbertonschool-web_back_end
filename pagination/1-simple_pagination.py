e3e2a1eb78d457c5e395107568baef34c7405b22
moha88z <mohammed.alayda88@gmail.com>
2025-10-21 11:05:31 +0000

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
-"""Simple pagination over Popular_Baby_Names.csv"""
+"""this module with afunction that returns an page index range"""
 
 import csv
+import math
 from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
 
 
 class Server:
     """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
+        """a function that returns the pages needed"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        data = self.dataset()
-        start, end = index_range(page, page_size)
+        if self.__dataset is None:
+            self.dataset()
 
-        if start >= len(data):
+        start_range, end_range = index_range(page, page_size)
+
+        if end_range > len(self.__dataset):
             return []
+        return self.__dataset[start_range:end_range]
 
-        return data[start:end]
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """a function that returns page index range"""
+    return ((page - 1) * page_size, page * page_size)
