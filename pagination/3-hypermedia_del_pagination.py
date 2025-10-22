ef18969b37b1377051ad542c2e56f17f2b2ab128
moha88z <mohammed.alayda88@gmail.com>
2025-10-22 14:26:54 +0000

    3-hypermedia_del_pagination.py

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+                }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """a method that returns data as a dict"""
+        assert isinstance(index, int)
+        assert 0 <= index < len(self.__indexed_dataset)
+
+        keys = sorted(self.__indexed_dataset.keys())
+
+        start_range = 0
+        while start_range < len(keys) and keys[start_range] < index:
+            start_range += 1
+
+        valid_keys = keys[start_range: start_range + page_size]
+
+        data_list = [self.__indexed_dataset[k] for k in valid_keys]
+        next_index = (
+            keys[start_range + page_size]
+            if start_range + page_size < len(keys)
+            else None
+        )
+
+        data_dict = {
+            "index": index,
+            "data": data_list,
+            "page_size": page_size,
+            "next_index": next_index,
+        }
+        return data_dict
